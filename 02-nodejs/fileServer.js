9b05763671fd93d2822b581a6431faad4c2a9d9f
vedansh <vedansh.dev2001@gmail.com>
2023-06-19 11:29:37 +0530

    fileServer.test.js completed

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -21,5 +21,31 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+app.get('/files',(req,res)=>{
+  fs.readdir(__dirname+'/files',(err,files)=>{
+    if(err){
+      res.send(500);
+    }
+    res.send(files);
+  })
+});
+
+
+app.get('/file/:filename',(req,res)=>{
+  const filePath = path.join(__dirname, '/files', req.params.filename);
+
+  fs.readFile(filePath,(err,data)=>{
+    if(err){
+      res.status(404).send("File not found");
+    }
+    res.status(200).send(data);
+  })
+})
+
+app.get('*',(req,res)=>{
+  res.status(404).send("Route not found");
+})
+
+// app.listen(3000);
 
 module.exports = app;
